832d54d5f13ebb83680b85f24ebf3110b7e54a0f
Rakshitha-32713305 <105207364+Rakshitha-32713305@users.noreply.github.com>
2022-06-21 12:49:03 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -35,10 +35,7 @@ void networkAlert_test (float celcius)
 
 void alertInCelcius(float farenheit)
 {
-    float celcius = (farenheit - 32) * 5 / 9;
-    
-#if (PRODUCTION_CODE_ENABLE != STD_ON)
-
+    float celcius = (farenheit - 32) * 5 / 9;    
     int returnCode = networkAlertStub(celcius);
     if (returnCode != 200)
     {
@@ -48,8 +45,8 @@ void alertInCelcius(float farenheit)
         // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount += 0;
     }
-    networkAlert_test(celcius);
-    
+#if (PRODUCTION_CODE_ENABLE != STD_ON)
+    networkAlert_test(celcius);    
 #endif
 
 }
