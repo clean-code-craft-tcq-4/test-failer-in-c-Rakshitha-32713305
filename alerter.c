f65c2bef0833cdcf5ddcf85d7ff63886f61b59b4
Rakshitha-32713305 <105207364+Rakshitha-32713305@users.noreply.github.com>
2022-06-27 23:36:07 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -1,35 +1,45 @@
 #include <stdio.h>
 #include <assert.h>
 
-#define PRODUCTION_CODE_ENABLE  STD_OFF 
+#define PRODUCTION_CODE_ENABLE  STD_ON 
 #define NETWORK_ALERT_THRESHOLDLEVEL 200
 
-int alertFailureCount = 0;
 
 #if (PRODUCTION_CODE_ENABLE == STD_OFF)
-
+int alertFailureCount = 0;
 int networkAlertStub(float celcius )
 {   
+    int ret =0;
     printf("ALERT: Temperature is %.1f celcius.\n", celcius);
+    if (celcius < NETWORK_ALERT_THRESHOLDLEVEL)
+    {
     // Return 200 for ok
+        ret = 200 ;
+    }
+    else 
+    {
     // Return 500 for not-ok
-    // stub always succeeds and returns 200    
-    return 200;
+      ret = 500 ;
+    }
+    return ret;
 }
 
-void networkAlert_test (float celcius)
+void networkAlert_belowthreshold_test (float celcius)
 {
     if (celcius < NETWORK_ALERT_THRESHOLDLEVEL)
          {
              assert(networkAlertStub(celcius)==200);
-         }
-    else
-        {
-             assert(networkAlertStub(celcius)==500);
-             assert(alertFailureCount != 0);    
-        }    
+         }      
 }
 
+void networkAlert_abovethreshold_test (float celcius)
+{
+    if (celcius >= NETWORK_ALERT_THRESHOLDLEVEL)
+         {
+            assert(networkAlertStub(celcius)==500);
+            assert(alertFailureCount != 0);    
+         }   
+}
 #endif
 
 void alertInCelcius(float farenheit)
@@ -45,7 +55,9 @@ void alertInCelcius(float farenheit)
         // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount += 0;
     }
-    networkAlert_test(celcius);    
+    networkAlert_abovethreshold_test(celcius);
+    networkAlert_belowthreshold_test(celcius); 
+    printf("%d alerts failed.\n", alertFailureCount);
 #endif
 
 }
@@ -53,8 +65,7 @@ void alertInCelcius(float farenheit)
 int main()
 {
     alertInCelcius(400.5);
-    alertInCelcius(303.6);
-    printf("%d alerts failed.\n", alertFailureCount);
+    alertInCelcius(303.6);   
     printf("All is well (maybe!)\n");
     return 0;
 }
